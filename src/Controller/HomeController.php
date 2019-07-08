e264f762382e24137cab0b72192502fa74df33f6
imbertjules <imbertjules@gmail.com>
2019-07-08 12:20:58 +0000

    crud des utilisateur telechargement de admin et creation de index et de contact

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,35 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+
+    /**
+     * @return Response
+     * @Route("/"")
+     */
+    public function index(): Response
+    {
+        //return new Response('<html><body>Bonjour ! </body></html>');
+        return $this->render('index.html.twig');
+    }
+
+    /**
+     * @Route("/contact", name="app_contact")
+     * @return Response
+     */
+    public function contact(): Response
+    {
+        return $this->render('contact.html.twig');
+    }
+
+
+
+}
